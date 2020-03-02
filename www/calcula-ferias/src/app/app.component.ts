5fac43f2f103e0d341d0cf877abac19781981374
Rafael Bernabeu <rbbernabeu@gmail.com>
2020-03-02 00:44:10 -0300

    update

--- www/calcula-ferias/src/app/app.component.ts
+++ www/calcula-ferias/src/app/app.component.ts
@@ -11,6 +11,8 @@ import {CalculaService} from "./calcula.service";
 export class AppComponent {
   title = 'calcula-ferias';
 
+  public periodos: number[] = [15, 10, 5];
+
   public dataInicial: Moment = moment("2020-07-01");
   public dataFinal: Moment = moment("2020-07-29");
   public selected: {startDate: Moment, endDate: Moment} = {startDate: this.dataInicial, endDate: this.dataFinal};
